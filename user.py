76934f5fe65e1384c1cf2876cb50f1c26e928eb5
Andrew59-boop <millerfaabi@gmail.com>
2020-04-20 14:16:51 +0300

    new change

--- user.py
+++ user.py
@@ -12,6 +12,35 @@ class User:
         self.login_username = login_username
         self.password= password
 
-     
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self) 
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self) 
+
+    @classmethod
+    def validate_user(cls,login_username,password):
+        '''
+        Method that takes in login_username and password
+        Args:
+            login_username : name
+            password :password
+        Returns :
+            password.
+        '''
+
+        for user in cls.user_list:
+            if user.login_username == login_username and user.password == password:
+                return password          
 
           
